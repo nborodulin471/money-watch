81af8a724aba6e511efdd74ca1a4ff7d3d534e3a
Бородулин Никита Петрович <nikita.borodulin@eltex.loc>
2025-04-20 20:35:40 +0200

    Initial commit

---
+++ src/main/java/ru/moneywatch/MoneyWatchApplication.java
@@ -0,0 +1,13 @@
+package ru.moneywatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyWatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyWatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/moneywatch/MoneyWatchApplicationTests.java
@@ -0,0 +1,14 @@
+package ru.moneywatch;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyWatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
